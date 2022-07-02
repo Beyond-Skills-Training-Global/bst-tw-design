a8853a82f3745da338bc789295d77db94f16be27
tesar-tech <jan@tesar.tech>
2022-07-02 18:20:28 +0200

    real testimonial data

--- wwwroot/js/data.js
+++ wwwroot/js/data.js
@@ -1,11 +1,44 @@
 let testimonials = [{
-    "name": "this is my name",
-    "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    "bio": "sdfsdf",
-    "img": "img/review.png"
-}, {
-    "name": "we",
-    "text": "d",
-    "bio": "hej I am bio",
-    "img": "img/review.png"
-}];
+        "name": "Petr Pech",
+        "text": "BST.COACH exercise library has allowed me to do more work and still feel good at the end of the day because I no longer need to demonstrate every exercise.",
+        "bio": "Strength & Conditioning coach of ATP & WTA Tennis Players",
+        "img": "img/review.png"
+    },
+    {
+        "name": "Blanka L. Navratilova",
+        "text": "Our dance club is spread across Europe and there is no way we can provide a consistent conditioning base to all our youth dancers in person. BST.COACH exercise video sharing solves it for us elegantly!",
+        "bio": "Mcgahan Lees Irish Dance Academy Europe",
+        "img": "img/review.png"
+    },
+    {
+        "name": "Adam Helcelet",
+        "text": "BST.COACH sharable exercise library allows us to bring consistency and athlete engagement across 30 youth sports summer camps.",
+        "bio": "Two-Time Olympic Decathlete | Patron at Kemp Vitezu Youth Sports Camps",
+        "img": "img/review.png"
+    },
+    {
+        "name": "Aaron Riekenberg",
+        "text": "I've personally witnessed Reinis overcome crippling injuries and accomplish impossible things. BST.COACH is a direct extension from his passion for sports and desire to help coaches.",
+        "bio": "Strength & Conditioning Coach at Konza Strength",
+        "img": "img/review.png"
+    },
+    {
+        "name": "Pauls Pujats",
+        "text": "If you're a coach or an athlete this platform is the ideal guide for getting clarity on your workouts and your training stage preparation. When I saw the amount of value this platform provides for FREE, I instantly realized that these guys are on a mission to change the world of youth sports for the better! ",
+        "bio": "Olympian | Sports Influencer | Trigger Point Therapy Specialist | Mindfulness & Yoga teacher ",
+        "img": "img/review.png"
+    },
+    {
+        "name": "Pavel Kalcev",
+        "text": "It has given my players an opportunity for consistent strength & conditioning training that otherwise would not be possible.",
+        "bio": "SaBaT U18 Head Baseball Coach",
+        "img": "img/review.png"
+    },
+    {
+        "name": "Sintija Grišule",
+        "text": "This platform has helped me build thought-through training plans for groups and individual athletes when I cannot be present. Very easy to create, save and send training.",
+        "bio": "Youth Track & Field Coach | Kuldiga, Latvia",
+        "img": "img/review.png"
+    },
+
+];
