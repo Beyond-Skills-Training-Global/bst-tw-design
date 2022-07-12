b4f16f83c3956bc50fd2d43d8b401588f0b6ef23
rajdevkar <rajdevkar@users.noreply.github.com>
2022-07-12 06:33:25 +0000

    Deploying to gh-pages from @ Beyond-Skills-Training-Global/bst-tw-design@7365b592175a745afc05c961977742c4e1adef07 🚀

--- js/data.js
+++ js/data.js
@@ -1,38 +1,163 @@
 let testimonials = [{
-        "name": "Petr Pech",
-        "text": "BST.COACH exercise library has allowed me to do more work and still feel good at the end of the day because I no longer need to demonstrate every exercise.",
-        "bio": "Strength & Conditioning coach of ATP & WTA Tennis Players",
-    },
-    {
-        "name": "Blanka L. Navratilova",
-        "text": "Our dance club is spread across Europe and there is no way we can provide a consistent conditioning base to all our youth dancers in person. BST.COACH exercise video sharing solves it for us elegantly!",
-        "bio": "Mcgahan Lees Irish Dance Academy Europe",
-    },
-    {
-        "name": "Adam Helcelet",
-        "text": "BST.COACH sharable exercise library allows us to bring consistency and athlete engagement across 30 youth sports summer camps.",
-        "bio": "Two-Time Olympic Decathlete | Patron at Kemp Vitezu Youth Sports Camps",
-    },
-    {
-        "name": "Aaron Riekenberg",
-        "text": "I've personally witnessed Reinis overcome crippling injuries and accomplish impossible things. BST.COACH is a direct extension from his passion for sports and desire to help coaches.",
-        "bio": "Strength & Conditioning Coach at Konza Strength",
-    },
-    {
-        "name": "Pauls Pujats",
-        "text": "If you're a coach or an athlete this platform is the ideal guide for getting clarity on your workouts and your training stage preparation. When I saw the amount of value this platform provides for FREE, I instantly realized that these guys are on a mission to change the world of youth sports for the better! ",
-        "bio": "Olympian | Sports Influencer | Trigger Point Therapy Specialist | Mindfulness & Yoga teacher ",
-        "imgMobile": "img/testimonials/testimonial05-mobile.jpg"
-    },
-    {
-        "name": "Pavel Kalcev",
-        "text": "It has given my players an opportunity for consistent strength & conditioning training that otherwise would not be possible.",
-        "bio": "SaBaT U18 Head Baseball Coach",
-    },
-    {
-        "name": "Sintija Grišule",
-        "text": "This platform has helped me build thought-through training plans for groups and individual athletes when I cannot be present. Very easy to create, save and send training.",
-        "bio": "Youth Track & Field Coach | Kuldiga, Latvia",
-    },
+    "name": "Petr Pech",
+    "text": "BST.COACH exercise library has allowed me to do more work and still feel good at the end of the day because I no longer need to demonstrate every exercise.",
+    "bio": "Strength & Conditioning coach of ATP & WTA Tennis Players",
+},
+{
+    "name": "Blanka L. Navratilova",
+    "text": "Our dance club is spread across Europe and there is no way we can provide a consistent conditioning base to all our youth dancers in person. BST.COACH exercise video sharing solves it for us elegantly!",
+    "bio": "Mcgahan Lees Irish Dance Academy Europe",
+},
+{
+    "name": "Adam Helcelet",
+    "text": "BST.COACH sharable exercise library allows us to bring consistency and athlete engagement across 30 youth sports summer camps.",
+    "bio": "Two-Time Olympic Decathlete | Patron at Kemp Vitezu Youth Sports Camps",
+},
+{
+    "name": "Aaron Riekenberg",
+    "text": "I've personally witnessed Reinis overcome crippling injuries and accomplish impossible things. BST.COACH is a direct extension from his passion for sports and desire to help coaches.",
+    "bio": "Strength & Conditioning Coach at Konza Strength",
+},
+{
+    "name": "Pauls Pujats",
+    "text": "If you're a coach or an athlete this platform is the ideal guide for getting clarity on your workouts and your training stage preparation. When I saw the amount of value this platform provides for FREE, I instantly realized that these guys are on a mission to change the world of youth sports for the better! ",
+    "bio": "Olympian | Sports Influencer | Trigger Point Therapy Specialist | Mindfulness & Yoga teacher ",
+    "imgMobile": "img/testimonials/testimonial05-mobile.jpg"
+},
+{
+    "name": "Pavel Kalcev",
+    "text": "It has given my players an opportunity for consistent strength & conditioning training that otherwise would not be possible.",
+    "bio": "SaBaT U18 Head Baseball Coach",
+},
+{
+    "name": "Sintija Grišule",
+    "text": "This platform has helped me build thought-through training plans for groups and individual athletes when I cannot be present. Very easy to create, save and send training.",
+    "bio": "Youth Track & Field Coach | Kuldiga, Latvia",
+},
 
 ];
+
+let FAQs = [
+    {
+        "title": "FAQ: BST COACH PLATFORM",
+        "qna": [
+            {
+                "question": "How is BST COACH better than the good-old pen and paper?",
+                "answer": "This seems obvious but it is actually a hard question to answer if we are very honest with ourselves. Writing a note to an athlete on a piece of paper wins the simplicity contest. Learning a new platform is unquestionably harder. But a piece of paper has its limits. It doesn't scale, it doesn't demonstrate exercises, it doesn't offer training ideas, and you cannot automate it.",
+            },
+            {
+                "question": "What type of coaches will benefit from BST COACH platform?",
+                "answer": "All coaches who know they cannot be with their athletes all the time. Our training platform can never replace a good coach because coaching means more than just writing a training plan, but we can be a great training tool that scales and makes the training process more consistent & engaging.",
+            },
+            {
+                "question": "What if I am a new coach and I don&apos;t really know how to build a good training plan?",
+                "answer": "Perfect! Then let us have the privilege to help you build a robust coaching mindset. We offer a Consultation service where we help guide new coaches through training program building.",
+            },
+            {
+                "question": "How can my athletes train with a phone in their hands?",
+                "answer": "They don't have to. It won't take very long until athletes will know the exercises by name. They won't even have to open most of the videos. The phone will only serve as a cheat sheet reminder. Athletes can also familiarize themselves with the training plan beforehand.",
+            },
+            {
+                "question": "Can athletes use BST COACH to create their own training?",
+                "answer": "For sure. Many athletes are extremely in tune with their own bodies, and they know what training they need for themselves.",
+            },
+            {
+                "question": "Can I use BST COACH for personal training purposes?",
+                "answer": "Yes. And very effectively for half the price you will find anywhere else. I dare you to test us!",
+            },
+            {
+                "question": "Does BST COACH offer games for the youngest athletes?",
+                "answer": "Not yet. However, we are planning to build cooperations with organizations who pursue exactly such things in the near future.",
+            },
+        ]
+    },
+    {
+        "title": "FAQ: BST LIBRARY & FEATURES",
+        "qna": [
+            {
+                "question": "What age range BST COACH exercises are intended for?",
+                "answer": "The critical age we focus on is 12-16 where most of the general athleticism foundations are built. We estimate that until about 12, athletes mostly prefer game-format training. However, our exercises have been effectively applied to train 7-year-olds and 70-year - olds alike.",
+            },
+            {
+                "question": "What equipment do my athletes need for BST COACH exercises?",
+                "answer": "Currently we offer minimal equipment exercise videos. Once in a while you might need a jump rope, stop watch, workout mat, resistance band, medicine ball, physio ball, tennis ball, boxes, hurdles, some light weights or other basic equipment… For the start, it will be plenty if your athletes are equipped with curiosity to learn and the will to become resilient athletes.",
+            },
+            {
+                "question": "What if some of my athletes cannot execute certain exercises?",
+                "answer": "Then you add Alternative Exercises to your plan. Athletes will see them in the plan as variations to the default exercises. We built this cool feature out of our own everyday coaching necessities.",
+            },
+            {
+                "question": "Can I add someone else's videos from YouTube or Vimeo?",
+                "answer": "Yes. But we strongly recommend that you contact the owner of the videos before using their resources or simply create your own video library on YouTube or Vimeo to later transfer over to BST COACH.",
+            },
+            {
+                "question": "Can I request the BST COACH team to film new exercises?",
+                "answer": "Actually yes! Just count that we will make them publicly available for everyone... Periodically we plan to film new exercises based on our knowledge and coaches&apos; requests. You will not miss when we post a new batch of videos!",
+            },
+            {
+                "question": "How can I deal with changing variables, such as training facilities, weather, equipment availability?",
+                "answer": "BST COACH lets you create 2 training plans under the same day: one regular plan and one Alternative Plan. Many of the bst library resources are designed for home-based training that require minimal space and equipment. Modified training is not skipped training.",
+            },
+            {
+                "question": "Can I hide certain training days from my athletes?",
+                "answer": "Yes, you can! It&apos;s one of coaches&apos; favorite features. Surprise workout feature. Unhide it right before the start of the training. Let your athletes sleep soundly and not overthink tomorrow&apos;s training plan.",
+            },
+        ]
+    },
+    {
+        "title": "FAQ: PLAN CREATION & SHARING",
+        "qna": [
+            {
+                "question": "Do I have to use videos to build my own plan?",
+                "answer": "No. You have complete freedom in how you communicate your training to your athletes. You can simply write text notes and describe what you want your athletes to do. Whatever is the most effective. Our videos will be mostly applied when the coach cannot be present or demonstrate the exercises.",
+            },
+            {
+                "question": "How many training plans can I create for my athletes?",
+                "answer": "As many as you wish. Price will not change. We have tested the platform with athletes at different age groups over a two-year period, and we found that saving each week's plan for the following cycle or even year, saves a lot of time and energy because coaches can recycle large parts of what they already made.",
+            },
+            {
+                "question": "What is the Group Page?",
+                "answer": "We have created a special extension to our platform where athletes will see all their plans and supplemental materials made only for their training group, accessible by the Group Key. Think of a homeroom for your athletes. It means that athletes neither have to register nor pay for the platform.",
+            },
+            {
+                "question": "How many athletes can be in one group?",
+                "answer": "Coach Plan allows access up to 20 athletes per Group Page. Team Plan is customized to your coaching scale and needs.",
+            },
+            {
+                "question": "How many groups can I have?",
+                "answer": "Coach Plan lets you simultaneously use up to 5 active training groups and Group Pages. You can always delete one group to create space for a new one. Team Plan is customized to your coaching scale and needs.",
+            },
+            {
+                "question": "What is Team Sharing and how to use it effectively?",
+                "answer": "Team Sharing is a unique feature for our Team Plan clients. It lets coaches within a sports organization or coaching cooperation share and exchange resources. Coaches can build and share training routines and plans that fit the overarching philosophy of the training group or organization.Each coach will have their own corner on the Group Page and every coach within the Team will be able to draw from all posted resources within the Group Page.",
+            }
+        ]
+    },
+    {
+        "title": "FAQ: ACCOUNT & PAYMENTS",
+        "qna": [
+            {
+                "question": "COMING SOON",
+                "answer": "",
+            },
+        ]
+    },
+    {
+        "title": "FAQ: PERSONAL TRAINING",
+        "qna": [
+            {
+                "question": "COMING SOON",
+                "answer": "",
+            },
+        ]
+    },
+    {
+        "title": "FAQ: COACHING CONSULTATIONS",
+        "qna": [
+            {
+                "question": "COMING SOON",
+                "answer": "",
+            },
+        ]
+    },
+]
