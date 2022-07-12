addb0d473d259abf809888934f5c3eeab44626e1
Raj Devkar <rajdevkar.6@gmail.com>
2022-07-12 11:48:00 +0530

    shifted commonly used js into app.js and removed unwanted js this fixes #26

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,3 +1,28 @@
+function toggleMenu() {
+    var h = document.getElementById("header");
+    var x = document.getElementById("menu");
+    var y = document.getElementById("openMenuSvg");
+    var z = document.getElementById("closeMenuSvg");
+
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "none";
+        h.style.background = "linear-gradient(rgba(0,0,0,1) 0%,rgba(0,0,0,0.8) 40%,rgba(0,0,0,0.3) 100%)";
+
+        a.style.display = "none";
+        b.style.display = "block";
+        c.style.display = "none";
+        d.style.marginTop = "16px";
+    } else {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "block";
+        h.style.background = "#262626";
+    }
+}
+
 function toggleSubMenu(dropdownId, menuId, menuCloseId, sectionMenuId) {
     var a = document.getElementById(dropdownId);
     var b = document.getElementById(menuId);
